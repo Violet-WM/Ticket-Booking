09f5d0fdbc24a81b607a355c77f7dc8c178b630b
Violet-WM <muiruri.violet21@students.dkut.ac.ke>
2024-06-11 18:30:15 +0300

    first commit

---
+++ app/src/main/java/com/example/ticketcard/ForgotPassword.java
@@ -0,0 +1,39 @@
+package com.example.ticketcard;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ForgotPassword extends AppCompatActivity {
+
+    private Button signInBtn, resetBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+
+        resetBtn = findViewById(R.id.resetBtn);
+        signInBtn = findViewById(R.id.signInBtn);
+
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ResetPassword.class));
+                finish();
+            }
+        });
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), login.class));
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/ticketcard/Home.java
@@ -0,0 +1,98 @@
+package com.example.ticketcard;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
+
+/*package com.example.ticketbooking;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+//import com.example.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import java.io.*;
+
+public class MainActivity extends AppCompatActivity
+        implements BottomNavigationView
+        .OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView
+                = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView
+                .setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.homef);
+    }
+    HomeFragment homeFragment = new HomeFragment();
+    TransactionsFragment  transactionsFragment = new  TransactionsFragment();
+    TicketsFragment ticketsFragment = new TicketsFragment();
+
+    SettingsFragment settingsFragment = new SettingsFragment();
+
+    @Override
+    public boolean
+    onNavigationItemSelected(@NonNull MenuItem item)
+    {
+
+        switch (item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, homeFragment)
+                        .commit();
+                return true;
+
+            case R.id.trans:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, transactionsFragment)
+                        .commit();
+                return true;
+
+            case R.id.ticko:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, ticketsFragment)
+                        .commit();
+                return true;
+
+            case R.id.settings:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, settingsFragment)
+                        .commit();
+                return true;
+        }
+        return false;
+    }
+}
+*/
