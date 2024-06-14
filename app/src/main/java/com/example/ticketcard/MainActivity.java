3bad80bca13fd46b7c9cdcafcf2d4253abfa06db
haronstan <haronstanley3@gmail.com>
2024-06-14 16:17:30 +0300

    Updated changes for Home Fragment, Home.Fragment.xml,EventsAdapter,model and its Event object,Admin Fragment, AdminFragment.xml,MainActivity xml eventscardview.xml,login and login.xml

--- app/src/main/java/com/example/ticketcard/MainActivity.java
+++ app/src/main/java/com/example/ticketcard/MainActivity.java
@@ -12,35 +12,33 @@ import androidx.appcompat.widget.Toolbar;
 
 // MainActivity class definition
 public class MainActivity extends AppCompatActivity {
-    // Declare a Button variable
+
+// Declare a Button variable
     private Button button;
 
-    // onCreate method is the entry point for the activity lifecycle
+// onCreate method is the entry point for the activity lifecycle
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize the button by finding it from the layout
+// Initialize the button by finding it from the layout
         button = findViewById(R.id.btn);
 
-        // Set an OnClickListener to the button to handle click events
+// Set an OnClickListener to the button to handle click events
         button.setOnClickListener(v -> {
-            // Create an Intent to navigate from MainActivity to login activity
+
+// Create an Intent to navigate from MainActivity to login activity
             Intent intent = new Intent(MainActivity.this, login.class);
 
-            // Start the login activity
+// Start the login activity
             startActivity(intent);
 
-            // Show a Toast message to the user
+// Show a Toast message to the user
             Toast.makeText(MainActivity.this, "Welcome, please Sign in.", Toast.LENGTH_SHORT).show();
 
-            // Finish the current activity to prevent the user from returning to it
+// Finish the current activity to prevent the user from returning to it
             finish();
         });
     }
-
-    // Inner class stanleyClass (currently empty, you might want to add functionality or remove it)
-   /* class stanley {
-    }*/
 }
