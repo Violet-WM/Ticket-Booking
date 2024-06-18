3ec7dd2e220ad6034c785eb4acab9aa94d878b70
tinegas <haron.tinega21@students.dkut.ac.ke>
2024-06-18 12:40:59 +0300

    AdminFragment.java updated

--- app/src/main/java/com/example/ticketcard/MainActivity.java
+++ app/src/main/java/com/example/ticketcard/MainActivity.java
@@ -79,9 +79,5 @@ public class MainActivity extends AppCompatActivity {
 //            }
 
         });
-    }
+    }}
 
-    // Inner class stanleyClass (currently empty, you might want to add functionality or remove it)
-   /* class stanley {
-    }*/
-}
