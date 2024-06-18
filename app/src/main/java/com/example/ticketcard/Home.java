72c295ccb38fa1ef01fb7a4afaa9480a6d76878b
victor <victorkimiti3760@gmail.com>
2024-06-18 03:18:57 +0300

    Persistent login enabled

--- app/src/main/java/com/example/ticketcard/Home.java
+++ app/src/main/java/com/example/ticketcard/Home.java
@@ -1,98 +1,59 @@
 package com.example.ticketcard;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.ticketcard.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class Home extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        replaceFragment(new HomeFragment());
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.home) {
+                    replaceFragment(new HomeFragment());
+                } else if (item.getItemId() == R.id.tickets) {
+                    replaceFragment(new TransactionsFragment());
+                } else if (item.getItemId() == R.id.transactions) {
+                    replaceFragment(new TicketsFragment());
+                } else if (item.getItemId() == R.id.settings) {
+                    replaceFragment(new SettingsFragment());
+                } else if (item.getItemId() == R.id.admin) {
+                    replaceFragment(new AdminFragment());
+                }
+                return false;
+            }
         });
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+
+        fragmentTransaction.commit();
     }
 }
-
-/*package com.example.ticketbooking;
-
-import android.os.Bundle;
-import android.view.MenuItem;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-//import com.example.Fragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import java.io.*;
-
-public class MainActivity extends AppCompatActivity
-        implements BottomNavigationView
-        .OnNavigationItemSelectedListener {
-
-    BottomNavigationView bottomNavigationView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        bottomNavigationView
-                = findViewById(R.id.bottomNavigationView);
-
-        bottomNavigationView
-                .setOnNavigationItemSelectedListener(this);
-        bottomNavigationView.setSelectedItemId(R.id.homef);
-    }
-    HomeFragment homeFragment = new HomeFragment();
-    TransactionsFragment  transactionsFragment = new  TransactionsFragment();
-    TicketsFragment ticketsFragment = new TicketsFragment();
-
-    SettingsFragment settingsFragment = new SettingsFragment();
-
-    @Override
-    public boolean
-    onNavigationItemSelected(@NonNull MenuItem item)
-    {
-
-        switch (item.getItemId()) {
-            case R.id.home:
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.frame_layout, homeFragment)
-                        .commit();
-                return true;
-
-            case R.id.trans:
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.frame_layout, transactionsFragment)
-                        .commit();
-                return true;
-
-            case R.id.ticko:
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.frame_layout, ticketsFragment)
-                        .commit();
-                return true;
-
-            case R.id.settings:
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.frame_layout, settingsFragment)
-                        .commit();
-                return true;
-        }
-        return false;
-    }
-}
-*/
--- app/src/main/java/com/example/ticketcard/football_categories.java
+++ app/src/main/java/com/example/ticketcard/football_categories.java
@@ -4,9 +4,6 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class football_categories extends AppCompatActivity {
 
@@ -14,6 +11,6 @@ public class football_categories extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_football_categories);
     }
 }
