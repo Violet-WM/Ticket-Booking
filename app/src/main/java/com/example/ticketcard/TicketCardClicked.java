d959382c4d5861c1c0fadba606f7422fae0f6823
tinegas <haron.tinega21@students.dkut.ac.ke>
2024-07-10 17:53:13 +0300

    ticketcard xml,

--- app/src/main/java/com/example/ticketcard/TicketCardClicked.java
+++ app/src/main/java/com/example/ticketcard/TicketCardClicked.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,12 +22,20 @@ import java.util.Random;
 
 public class TicketCardClicked extends AppCompatActivity {
 
+    ImageView arrow;
+    Button bookYourSeat;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_card_clicked);
 
-        Button bookYourSeat = findViewById(R.id.bookYourSeat);
+        arrow = findViewById(R.id.ticketcardbackarrow);
+        arrow.setOnClickListener(v -> {
+            Intent intent = new Intent(this, FkfPremierLeague.class);
+                });
+
+        bookYourSeat = findViewById(R.id.bookYourSeat);
         bookYourSeat.setOnClickListener(v -> {
             Intent intent = new Intent(this, Buyticket.class);
             startActivity(intent);
