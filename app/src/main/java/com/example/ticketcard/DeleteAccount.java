90ae6cec0de2abe0574bfdc35dc815ef61f2cfa9
victor <victorkimiti3760@gmail.com>
2024-08-07 16:16:55 +0300

    Delete account

--- app/src/main/java/com/example/ticketcard/DeleteAccount.java
+++ app/src/main/java/com/example/ticketcard/DeleteAccount.java
@@ -1,7 +1,10 @@
 package com.example.ticketcard;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class DeleteAccount extends AppCompatActivity {
 
     String currentUsername;
+    Button deleteAccount;
+    DatabaseReference usersRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +31,26 @@ public class DeleteAccount extends AppCompatActivity {
             return insets;
         });
 
+        deleteAccount = findViewById(R.id.deleteAccount);
+
         // Retrieve the user name from SharedPreferences
         SharedPreferences sharedPreferences = getSharedPreferences("UserPrefs", MODE_PRIVATE);
         currentUsername = sharedPreferences.getString("userName", "user"); // "user" is the default value if "userName" is not found
+
+        usersRef = FirebaseDatabase.getInstance().getReference("Users_info").child(currentUsername);
+
+        deleteAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                usersRef.removeValue();
+                deleteSharedPreferences();
+                startActivity(new Intent(DeleteAccount.this, SignUpActivity.class));
+            }
+        });
+    }
+
+    private void deleteSharedPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences("UserPrefs", MODE_PRIVATE);
+        sharedPreferences.edit().clear().commit();
     }
 }
